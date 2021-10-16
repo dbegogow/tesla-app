6b4f65ea46b9809e52379fb0a2e963e47fb9834c
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-16 21:46:56 +0300

    Create car item component styles

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import styles from './styles';
 
 const CarItem = () => {
     return (
-        <View>
-            <View>
-                
+        <View style={styles.carContainer}>
+            <View style={styles.header}>
+                <Text>Settings</Text>
+                <Text>Tesla Model S</Text>
+                <Text>Surprise</Text>
             </View>
         </View>
     );
---
+++ components/CarItem/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    carContainer: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'orange'
+    },
+    header: {
+        
+    }
+});
+
+export default styles;
