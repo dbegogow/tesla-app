e52a5465e60d08f70fb881831413c1b4242de468
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-16 22:35:10 +0300

    Add tool box icon in the header

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faToolbox } from '@fortawesome/free-solid-svg-icons';
 
 const CarItem = () => {
     return (
         <View style={styles.carContainer}>
             <View style={styles.header}>
                 <FontAwesomeIcon icon={faCog} />
-                <Text>Settings</Text>
                 <Text>Tesla Model S</Text>
-                <Text>Surprise</Text>
+                <FontAwesomeIcon icon={faToolbox} />
             </View>
         </View>
     );
